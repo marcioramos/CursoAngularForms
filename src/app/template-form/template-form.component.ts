46cf9a1d19d3115ad70ee886485383cd24b94722
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-05-30 13:09:12 -0300

    Aula 85

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-template-form',
@@ -17,7 +19,7 @@ export class TemplateFormComponent implements OnInit {
   //   email: 'loiane@email.com'
   // };
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
@@ -27,7 +29,7 @@ export class TemplateFormComponent implements OnInit {
     //console.log(this.usuario);
   }
 
-  verificaValidTouched(campo){
+  verificaValidTouched(campo) {
     return !campo.valid && campo.touched;
   }
 
@@ -36,6 +38,33 @@ export class TemplateFormComponent implements OnInit {
       'has-error': this.verificaValidTouched(campo),
       'has-feedback': this.verificaValidTouched(campo)
     }
+  }
+
+  consultaCEP(cep) {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .map(dados => dados.json())
+          .subscribe(dados => console.log(dados));
+        
+
+      }
+
+    }
+
+
+
   }
 
 }
