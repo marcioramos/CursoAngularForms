7df8fa207efc7bac07c20325442153beac15739b
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-05-30 14:20:30 -0300

    Aula 86

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -40,31 +40,66 @@ export class TemplateFormComponent implements OnInit {
     }
   }
 
-  consultaCEP(cep) {
+  consultaCEP(cep, form) {
 
     //Nova variável "cep" somente com dígitos.
     var cep = cep.replace(/\D/g, '');
 
     //Verifica se campo cep possui valor informado.
     if (cep != "") {
-
       //Expressão regular para validar o CEP.
       var validacep = /^[0-9]{8}$/;
 
       //Valida o formato do CEP.
       if (validacep.test(cep)) {
 
+        this.resetaDadosForm(form);
+
         this.http.get(`//viacep.com.br/ws/${cep}/json`)
           .map(dados => dados.json())
-          .subscribe(dados => console.log(dados));
-        
-
+          .subscribe(dados => this.populaDadosForm(dados, form));
       }
-
     }
+  }
 
+  populaDadosForm(dados, form) {
+    /*form.setValue({
+      nome: form.value.nome,
+      email: form.value.email,
+      endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,
+        numero: '',
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });*/
 
+    form.form.patchValue({
+      endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
 
+  resetaDadosForm(form) {
+    form.form.patchValue({
+      endereco: {
+        cep: null,
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
 }
