47efcd41c14fd503a257958d34e024a7b6113f3d
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-07-17 21:51:27 -0300

    Aula 95

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,50 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
-import { Http } from '@angular/http';
-
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
+import { Http } from "@angular/http";
 
 @Component({
-  selector: 'app-data-form',
-  templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.css']
+  selector: "app-data-form",
+  templateUrl: "./data-form.component.html",
+  styleUrls: ["./data-form.component.css"]
 })
 export class DataFormComponent implements OnInit {
-
   formulario: FormGroup;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private http: Http
-    ) { }
+  constructor(private formBuilder: FormBuilder, private http: Http) {}
 
   ngOnInit() {
-
     /*this.formulario = new FormGroup({
       nome: new FormControl(null),
       email: new FormControl(null)
     });*/
 
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, [Validators.required, Validators.minLength(3)]],
+      email: [null, [Validators.required, Validators.email]]
     });
+  }
 
+  verificaValidTouched(nomeCampo) {
+    const campo = this.formulario.get(nomeCampo);
+    return !campo.valid && campo.touched;
+  }
+
+  verificaEmailInvalido() {
+    const campo = this.formulario.get('email');
+
+    if (campo.errors) {
+      return campo.errors['email'] && campo.touched;
+    }
+  }
+
+  aplicaCssErro(nomeCampo) {
+    return {
+      'has-error': this.verificaValidTouched(nomeCampo),
+      'has-feedback': this.verificaValidTouched(nomeCampo)
+    }
   }
 
   onSubmit() {
     console.log(this.formulario.value);
 
-    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
-    .map(res => res)
-    .subscribe(dados => {
-      console.log(dados);
-      this.formulario.reset();
-    },
-      (error: any) => alert('erro'));
+    if (!this.formulario.valid){
+      alert('Dados Inválidos');
+      return;
+    }
+
+    this.http
+      .post("https://httpbin.org/post", JSON.stringify(this.formulario.value))
+      .map(res => res)
+      .subscribe(
+        dados => {
+          console.log(dados);
+          this.formulario.reset();
+        },
+        (error: any) => alert("erro")
+      );
   }
 
   resetar() {
     this.formulario.reset();
   }
-
 }
